995c4dd7c2a53e6141e5c8d6f60057034899f14e
kareem <kshaik_sr2021@yahoo.com>
2025-10-19 22:32:52 +0100

    Template literals in React.JS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const main = {
+  background: 'black', 
+  height: '100vh', 
+  width: '100%'
+}
+
+const firstvariable = 'Expert'
+const secondvariable = 'Developer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<>
+  <div style={main}>
+    <h1 style={{color: 'green'}}>
+      Shaik Solutions Private Limited
+    </h1>
+    <h2 style={{color: 'yellow'}}>
+      {`We are ${firstvariable} ${secondvariable}`}
+    </h2>
+  </div>
+</>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
